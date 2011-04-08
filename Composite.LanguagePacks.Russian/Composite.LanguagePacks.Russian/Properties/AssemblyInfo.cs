79c1aabd22e5f7955c452886be5dc58568400252
Volodymyr I. Muzyka <info@aeont.com>
2011-04-08 09:32:11 +0200

    [Composite.LanguagePacks.Russian] 1.0.14: prepare for CodePlex

---
+++ Composite.LanguagePacks.Russian/Composite.LanguagePacks.Russian/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Composite.LanguagePacks.Russian")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Composite C1")]
+[assembly: AssemblyProduct("Composite.LanguagePacks.Russian")]
+[assembly: AssemblyCopyright("Copyright © Composite C1 2011")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("cfe219d3-ac46-4c0e-80ff-afd4cc9011c6")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
