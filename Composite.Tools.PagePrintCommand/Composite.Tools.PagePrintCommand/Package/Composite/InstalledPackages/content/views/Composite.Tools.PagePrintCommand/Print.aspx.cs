7ac98ac0b3336b1be1ac18a02185beb33e82f4d0
Volodymyr I. Muzyka <info@aeont.com>
2011-04-11 10:37:48 +0200

    [Composite.Tools.PagePrintCommand] prepare for CodePlex

---
+++ Composite.Tools.PagePrintCommand/Composite.Tools.PagePrintCommand/Package/Composite/InstalledPackages/content/views/Composite.Tools.PagePrintCommand/Print.aspx.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Composite.Core.WebClient.Renderings.Page;
+
+public partial class Composite_InstalledPackages_content_views_Composite_Administration_Print_Print : System.Web.UI.Page
+{
+	protected void Page_Load(object sender, EventArgs e)
+	{
+		var pageId = Guid.Parse(this.Request["id"].ToString());
+		var pageUrl = string.Empty;
+		if (PageStructureInfo.TryGetPageUrl(pageId, out pageUrl))
+		{
+			lPrintFrame.Text = string.Format(@"<iframe src=""{0}"" id=""printFrame"" style=""width:100%;height:100%;background-color:white;"" onload=""PrintFrame();"" ></iframe>", pageUrl);
+		}
+	}
+}
