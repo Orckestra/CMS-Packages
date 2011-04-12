6b63eacee9033296bb38ab5300ed698f36f23db7
Volodymyr I. Muzyka <info@aeont.com>
2011-04-12 10:48:16 +0200

    [Composite.Tools.LegacyUrlHandler] 1.0.9: package description

--- Composite.Tools.LegacyUrlHandler/Composite.Tools.LegacyUrlHandler/Properties/AssemblyInfo.cs
+++ Composite.Tools.LegacyUrlHandler/Composite.Tools.LegacyUrlHandler/Properties/AssemblyInfo.cs
@@ -8,9 +8,9 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("Composite.Tools.LegacyUrlHandler")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Composite A/S")]
 [assembly: AssemblyProduct("Composite.Tools.LegacyUrlHandler")]
-[assembly: AssemblyCopyright("Copyright ©  2010")]
+[assembly: AssemblyCopyright("Copyright ©  2011")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.9")]
+[assembly: AssemblyFileVersion("1.0.9")]
