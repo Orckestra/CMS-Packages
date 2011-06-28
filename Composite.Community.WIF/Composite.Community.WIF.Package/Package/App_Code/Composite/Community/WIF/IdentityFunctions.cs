8f5fa0fdf6b230a9718135cdbb972a329a2a98bb
Dmitry Dzygin <dmitriy.dzygin@gmail.com>
2011-06-28 15:05:58 +0200

    [Composite.Community.WIF] v1.0.1: Initial check-in

---
+++ Composite.Community.WIF/Composite.Community.WIF.Package/Package/App_Code/Composite/Community/WIF/IdentityFunctions.cs
@@ -0,0 +1,90 @@
+﻿using System.Linq;
+using System.Reflection;
+using System.Threading;
+using System.Web;
+using System.Web.Security;
+using System.Web.UI;
+using Microsoft.IdentityModel.Claims;
+using Microsoft.IdentityModel.Web;
+using Microsoft.IdentityModel.Web.Controls;
+
+namespace Composite.Community.WIF
+{
+    public class IdentityFunctions
+    {
+        private static ClaimsIdentity CurrentIdentity
+        {
+            get { return Thread.CurrentPrincipal.Identity as ClaimsIdentity; }
+        }
+
+        public static bool IsLoggedIn()
+        {
+            return CurrentIdentity != null && CurrentIdentity.Claims.Count > 0 /* !string.IsNullOrEmpty(CurrentIdentity.Name)*/;
+        }
+
+        public static void RedirectToLogin()
+        {
+            (HttpContext.Current.Handler as Page).PreRender += (o, args) => RedirectToLoginInt();
+        }
+
+        public static void RedirectToLoginInt()
+        {
+            var btn = new FederatedPassiveSignIn();
+            btn.UseFederationPropertiesFromConfiguration = true;
+            btn.Page = (HttpContext.Current.Handler as Page);
+
+            BindingFlags instanceNotPublic = BindingFlags.Instance | BindingFlags.NonPublic;
+
+            typeof(SignInControl)
+                .GetMethod("CreateChildControls", instanceNotPublic)
+                .Invoke(btn, new object[0]);
+
+            typeof(FederatedPassiveSignIn)
+                .GetMethod("CreateWSFederationAuthenticationModule", instanceNotPublic)
+                .Invoke(btn, new object[0]);
+
+            typeof(FederatedPassiveSignIn)
+                .GetMethod("OnClick", instanceNotPublic)
+                .Invoke(btn, new object[] { null, null });
+        }
+
+
+        public static void LogOut()
+        {
+            (HttpContext.Current.Handler as Page).PreRender += (o, args) => LogOutInt();
+        }
+
+        public static void LogOutInt()
+        {
+            FormsAuthentication.SignOut();
+            FederatedAuthentication.SessionAuthenticationModule.DeleteSessionTokenCookie();
+
+            var context = HttpContext.Current;
+            context.Response.Redirect(context.Request.RawUrl);
+        }
+
+	    public static string GetName()
+	    {
+	        var identity = Thread.CurrentPrincipal.Identity as ClaimsIdentity;
+            if (identity == null) return string.Empty;
+
+            if (identity.Name != null) return identity.Name;
+
+	        var emailClaim = identity.Claims.FirstOrDefault(c => c.ClaimType == ClaimTypes.Email);
+            if(emailClaim != null) return emailClaim.Value;
+
+            var nameIdentifierClaim = identity.Claims.FirstOrDefault(c => c.ClaimType == ClaimTypes.NameIdentifier);
+            if (nameIdentifierClaim != null) return "ID: " + nameIdentifierClaim.Value;
+
+            return string.Empty;
+	    }
+
+        public static void DisableAspNetCache()
+        {
+            var context = HttpContext.Current;
+            if (context == null) return;
+
+            context.Response.Cache.SetCacheability(HttpCacheability.NoCache);
+        }
+    }
+}
---
+++ Composite.Community.WIF/Composite.Community.WIF.Package/Package/App_Data/Xslt/Composite/Community/WIF/LoginForm.xsl
@@ -0,0 +1,77 @@
+﻿<?xml version="1.0" encoding="UTF-8"?>
+<xsl:stylesheet version="1.0" xmlns:xsl="http://www.w3.org/1999/XSL/Transform"
+  xmlns:in="http://www.composite.net/ns/transformation/input/1.0"
+  xmlns:lang="http://www.composite.net/ns/localization/1.0"
+  xmlns:f="http://www.composite.net/ns/function/1.0"
+  xmlns:c1="http://c1.composite.net/StandardFunctions"
+  xmlns:msxsl="urn:schemas-microsoft-com:xslt" 
+  xmlns:csharp="http://c1.composite.net/sample/csharp"
+  xmlns="http://www.w3.org/1999/xhtml"
+  exclude-result-prefixes="xsl in lang f msxsl csharp">
+    
+  <xsl:variable name="IsLoggedIn" select="count(/in:inputs/in:result[@name='IsLoggedIn'][text()='true']) > 0" />
+  <xsl:variable name="UserName" select="/in:inputs/in:result[@name='GetName']/text()" />
+    
+  <xsl:template match="/">
+    <html>
+      <head>
+      </head>
+
+      <body>
+        <form method="post">
+          
+              <xsl:choose>
+                <xsl:when test="$IsLoggedIn">
+                   Logged in as <xsl:value-of select="$UserName" />
+                  
+                   <input type="submit" name="wif_logout" value="Log out" />
+                </xsl:when>
+                <xsl:otherwise>
+        
+                   <input type="submit" name="wif_login" value="Log in" />
+                </xsl:otherwise>
+              </xsl:choose>
+          
+         </form>
+        
+        <xsl:if test="c1:GetFormData('wif_login') != ''">
+          <xsl:copy-of select="csharp:RedirectToLogin()" />
+        </xsl:if>
+        
+        <xsl:if test="c1:GetFormData('wif_logout') != ''">
+          <xsl:copy-of select="csharp:LogOut()" />
+        </xsl:if>
+
+        <xsl:copy-of select="csharp:DisableAspNetCache()" />
+      </body>
+    </html>
+  </xsl:template>
+
+  <msxsl:script implements-prefix="csharp" language="C#"> 
+     
+    <msxsl:assembly name="App_Code" /> 
+      
+    <![CDATA[
+        public static string RedirectToLogin()
+        {
+            Composite.Community.WIF.IdentityFunctions.RedirectToLogin();
+            return string.Empty;
+        }
+
+        public static string LogOut()
+        {
+            Composite.Community.WIF.IdentityFunctions.LogOut();
+            return string.Empty;
+        }
+
+        public static string DisableAspNetCache()
+        {
+            Composite.Community.WIF.IdentityFunctions.DisableAspNetCache();
+            return string.Empty;
+        }
+    ]]>
+   </msxsl:script> 
+    
+   
+    
+</xsl:stylesheet>
