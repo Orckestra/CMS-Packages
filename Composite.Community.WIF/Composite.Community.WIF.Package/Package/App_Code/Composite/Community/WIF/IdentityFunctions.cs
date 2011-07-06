488cc5048df44605f3385d0a8c1a7ac32baf5f00
Volodymyr I. Muzyka <info@aeont.com>
2011-07-06 11:34:12 +0200

    [Composite.Community.WIF] 1.0.3: project and package renamed from Composite.Community.WindowsIdentityFoundation to have single naming everywhere

--- Composite.Community.WIF/Composite.Community.WIF.Package/Package/App_Code/Composite/Community/WIF/IdentityFunctions.cs
+++ Composite.Community.WIF/Composite.Community.WIF.Package/Package/App_Code/Composite/Community/WIF/IdentityFunctions.cs
@@ -10,81 +10,81 @@ using Microsoft.IdentityModel.Web.Controls;
 
 namespace Composite.Community.WIF
 {
-    public class IdentityFunctions
-    {
-        private static ClaimsIdentity CurrentIdentity
-        {
-            get { return Thread.CurrentPrincipal.Identity as ClaimsIdentity; }
-        }
+	public class IdentityFunctions
+	{
+		private static ClaimsIdentity CurrentIdentity
+		{
+			get { return Thread.CurrentPrincipal.Identity as ClaimsIdentity; }
+		}
 
-        public static bool IsLoggedIn()
-        {
-            return CurrentIdentity != null && CurrentIdentity.Claims.Count > 0 /* !string.IsNullOrEmpty(CurrentIdentity.Name)*/;
-        }
+		public static bool IsLoggedIn()
+		{
+			return CurrentIdentity != null && CurrentIdentity.Claims.Count > 0 /* !string.IsNullOrEmpty(CurrentIdentity.Name)*/;
+		}
 
-        public static void RedirectToLogin()
-        {
-            (HttpContext.Current.Handler as Page).PreRender += (o, args) => RedirectToLoginInt();
-        }
+		public static void RedirectToLogin()
+		{
+			(HttpContext.Current.Handler as Page).PreRender += (o, args) => RedirectToLoginInt();
+		}
 
-        public static void RedirectToLoginInt()
-        {
-            var btn = new FederatedPassiveSignIn();
-            btn.UseFederationPropertiesFromConfiguration = true;
-            btn.Page = (HttpContext.Current.Handler as Page);
+		public static void RedirectToLoginInt()
+		{
+			var btn = new FederatedPassiveSignIn();
+			btn.UseFederationPropertiesFromConfiguration = true;
+			btn.Page = (HttpContext.Current.Handler as Page);
 
-            BindingFlags instanceNotPublic = BindingFlags.Instance | BindingFlags.NonPublic;
+			BindingFlags instanceNotPublic = BindingFlags.Instance | BindingFlags.NonPublic;
 
-            typeof(SignInControl)
-                .GetMethod("CreateChildControls", instanceNotPublic)
-                .Invoke(btn, new object[0]);
+			typeof(SignInControl)
+				.GetMethod("CreateChildControls", instanceNotPublic)
+				.Invoke(btn, new object[0]);
 
-            typeof(FederatedPassiveSignIn)
-                .GetMethod("CreateWSFederationAuthenticationModule", instanceNotPublic)
-                .Invoke(btn, new object[0]);
+			typeof(FederatedPassiveSignIn)
+				.GetMethod("CreateWSFederationAuthenticationModule", instanceNotPublic)
+				.Invoke(btn, new object[0]);
 
-            typeof(FederatedPassiveSignIn)
-                .GetMethod("OnClick", instanceNotPublic)
-                .Invoke(btn, new object[] { null, null });
-        }
+			typeof(FederatedPassiveSignIn)
+				.GetMethod("OnClick", instanceNotPublic)
+				.Invoke(btn, new object[] { null, null });
+		}
 
 
-        public static void LogOut()
-        {
-            (HttpContext.Current.Handler as Page).PreRender += (o, args) => LogOutInt();
-        }
+		public static void LogOut()
+		{
+			(HttpContext.Current.Handler as Page).PreRender += (o, args) => LogOutInt();
+		}
 
-        public static void LogOutInt()
-        {
-            FormsAuthentication.SignOut();
-            FederatedAuthentication.SessionAuthenticationModule.DeleteSessionTokenCookie();
+		public static void LogOutInt()
+		{
+			FormsAuthentication.SignOut();
+			FederatedAuthentication.SessionAuthenticationModule.DeleteSessionTokenCookie();
 
-            var context = HttpContext.Current;
-            context.Response.Redirect(context.Request.RawUrl);
-        }
+			var context = HttpContext.Current;
+			context.Response.Redirect(context.Request.RawUrl);
+		}
 
-	    public static string GetName()
-	    {
-	        var identity = Thread.CurrentPrincipal.Identity as ClaimsIdentity;
-            if (identity == null) return string.Empty;
+		public static string GetName()
+		{
+			var identity = Thread.CurrentPrincipal.Identity as ClaimsIdentity;
+			if (identity == null) return string.Empty;
 
-            if (identity.Name != null) return identity.Name;
+			if (identity.Name != null) return identity.Name;
 
-	        var emailClaim = identity.Claims.FirstOrDefault(c => c.ClaimType == ClaimTypes.Email);
-            if(emailClaim != null) return emailClaim.Value;
+			var emailClaim = identity.Claims.FirstOrDefault(c => c.ClaimType == ClaimTypes.Email);
+			if (emailClaim != null) return emailClaim.Value;
 
-            var nameIdentifierClaim = identity.Claims.FirstOrDefault(c => c.ClaimType == ClaimTypes.NameIdentifier);
-            if (nameIdentifierClaim != null) return "ID: " + nameIdentifierClaim.Value;
+			var nameIdentifierClaim = identity.Claims.FirstOrDefault(c => c.ClaimType == ClaimTypes.NameIdentifier);
+			if (nameIdentifierClaim != null) return "ID: " + nameIdentifierClaim.Value;
 
-            return string.Empty;
-	    }
+			return string.Empty;
+		}
 
-        public static void DisableAspNetCache()
-        {
-            var context = HttpContext.Current;
-            if (context == null) return;
+		public static void DisableAspNetCache()
+		{
+			var context = HttpContext.Current;
+			if (context == null) return;
 
-            context.Response.Cache.SetCacheability(HttpCacheability.NoCache);
-        }
-    }
+			context.Response.Cache.SetCacheability(HttpCacheability.NoCache);
+		}
+	}
 }
