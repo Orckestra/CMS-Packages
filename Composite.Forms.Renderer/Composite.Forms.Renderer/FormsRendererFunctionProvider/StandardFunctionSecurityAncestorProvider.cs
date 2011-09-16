16d113baa18857daa61a25b55969099c38f64581
Inna Boitsun <inna.boytsun.85@gmail.com>
2011-09-16 09:45:45 +0200

    [Composite.Forms.Renderer] 1.4.3 , minimimCompositeVersion="2.1.4113.99" - copied the class Composite.Functions.StandardFunctionSecurityAncestorProvider to the Package project (temporary workaround).

---
+++ Composite.Forms.Renderer/Composite.Forms.Renderer/FormsRendererFunctionProvider/StandardFunctionSecurityAncestorProvider.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Composite.C1Console.Security;
+using Composite.Plugins.Elements.ElementProviders.BaseFunctionProviderElementProvider;
+
+namespace Composite.Forms.Renderer.FormsRendererFunctionProvider
+{
+#warning This class is a work around, duplicating code from C1 3.0. Remove this class when building a C1 3.0 only release of this package – instead call Composite.Functions.StandardFunctionSecurityAncestorProvider
+	public class StandardFunctionSecurityAncestorProvider : ISecurityAncestorProvider
+	{
+		public IEnumerable<EntityToken> GetParents(EntityToken entityToken)
+		{
+			string fullname = entityToken.Id;
+			string providerName = entityToken.Source;
+
+			if (fullname.Contains('.'))
+			{
+				fullname = fullname.Remove(fullname.LastIndexOf('.'));
+			}
+
+			string id = BaseFunctionProviderElementProvider.CreateId(fullname, "AllFunctionsElementProvider");
+
+			yield return new BaseFunctionFolderElementEntityToken(id);
+		}
+	}
+}
