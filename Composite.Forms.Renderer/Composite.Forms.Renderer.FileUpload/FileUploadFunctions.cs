ae52dd207ff72264d7cc14ef217d911def5164e1
Taras Nakonechnyi <neexite@gmail.com>
2011-11-22 13:51:27 +0100

    [Composite.Forms.Renderer.FileUpload] 1.2.3 Fix attachment with empty field

--- Composite.Forms.Renderer/Composite.Forms.Renderer.FileUpload/FileUploadFunctions.cs
+++ Composite.Forms.Renderer/Composite.Forms.Renderer.FileUpload/FileUploadFunctions.cs
@@ -25,94 +25,104 @@ namespace Composite.Forms.Renderer.FileUpload
 		[FunctionParameterDescription("Subject", "Subject", "Subject")]
 		public static string SendEmail(string ResponseText, string From, string To, string Cc, string Subject)
 		{
-			var newDataField = typeof(FormsRenderer).GetField("newData", BindingFlags.NonPublic | BindingFlags.Static);
-			IData newData = (IData)newDataField.GetValue(null);
-
-			var page = HttpContext.Current.Handler as Page;
-			var attachments = new List<Attachment>();
-			if (page != null)
+			try
 			{
-				foreach (string fileName in page.Request.Files)
+				var newDataField = typeof (FormsRenderer).GetField("newData", BindingFlags.NonPublic | BindingFlags.Static);
+				IData newData = (IData) newDataField.GetValue(null);
+
+				var page = HttpContext.Current.Handler as Page;
+				var attachments = new List<Attachment>();
+				if (page != null)
 				{
-					var file = page.Request.Files[fileName];
+					foreach (string fileName in page.Request.Files)
+					{
+						var file = page.Request.Files[fileName];
 
-					if (file.FileName == string.Empty) continue;
+						if (file.FileName == string.Empty) continue;
 
-					var filename = NormalizeFilename(file.FileName);
+						var filename = NormalizeFilename(Path.GetFileName(file.FileName));
 
-					attachments.Add(new Attachment(file.InputStream, filename, file.ContentType)
-					{ 
-						NameEncoding = Encoding.ASCII
-					});
+						attachments.Add(new Attachment(file.InputStream, filename, file.ContentType)
+						                	{
+						                		NameEncoding = Encoding.ASCII
+						                	});
+					}
 				}
-			}
 
-			XElement inputXml = FormsRenderer.GetXElement(newData);
-			XDocument mailBody = new XDocument();
+				XElement inputXml = FormsRenderer.GetXElement(newData);
+				XDocument mailBody = new XDocument();
 
-			XslCompiledTransform xslTransform = new XslCompiledTransform();
-			xslTransform.Load(FormsRenderer.FormsRendererLocalPath + "Xslt/MailBody.xslt");
+				XslCompiledTransform xslTransform = new XslCompiledTransform();
+				xslTransform.Load(FormsRenderer.FormsRendererLocalPath + "Xslt/MailBody.xslt");
 
-			using (var writer = mailBody.CreateWriter())
-			{
-				xslTransform.Transform(inputXml.CreateReader(), writer);
-			}
+				using (var writer = mailBody.CreateWriter())
+				{
+					xslTransform.Transform(inputXml.CreateReader(), writer);
+				}
 
-			MailMessage msgMail = new MailMessage();
-			try
-			{
-				msgMail.From = new MailAddress(From);
-			}
-			catch (Exception e)
-			{
-				LoggingService.LogError(string.Format("Mail sending(From: '{0}')", From), e.Message);
-				return string.Empty;
-			}
-			try
-			{
-				msgMail.To.Add(To);
-			}
-			catch (Exception e)
-			{
-				LoggingService.LogError(string.Format("Mail sending(To: '{0}')", To), e.Message);
-				return string.Empty;
-			}
-			if (!string.IsNullOrEmpty(Cc))
-			{
+				MailMessage msgMail = new MailMessage();
 				try
 				{
-					msgMail.CC.Add(Cc);
+					msgMail.From = new MailAddress(From);
 				}
 				catch (Exception e)
 				{
-					LoggingService.LogError(string.Format("Mail sending(Cc: '{0}')", Cc), e.Message);
+					LoggingService.LogError(string.Format("Mail sending(From: '{0}')", From), e.Message);
+					throw e;
 				}
-			}
-
-			foreach (var attachment in attachments)
-			{
 				try
 				{
-					msgMail.Attachments.Add(attachment);
+					msgMail.To.Add(To);
 				}
 				catch (Exception e)
 				{
-					LoggingService.LogError(string.Format("Mail sending(Attachment: '{0}')", attachment.Name), e.Message);
+					LoggingService.LogError(string.Format("Mail sending(To: '{0}')", To), e.Message);
+					throw e;
 				}
-			}
+				if (!string.IsNullOrEmpty(Cc))
+				{
+					try
+					{
+						msgMail.CC.Add(Cc);
+					}
+					catch (Exception e)
+					{
+						LoggingService.LogError(string.Format("Mail sending(Cc: '{0}')", Cc), e.Message);
+						throw e;
+					}
+				}
 
-			try
-			{
-				msgMail.Subject = Subject;
-				msgMail.IsBodyHtml = true;
-				msgMail.Body = mailBody.ToString();
+				foreach (var attachment in attachments)
+				{
+					try
+					{
+						msgMail.Attachments.Add(attachment);
+					}
+					catch (Exception e)
+					{
+						LoggingService.LogError(string.Format("Mail sending(Attachment: '{0}')", attachment.Name), e.Message);
+						throw e;
+					}
+				}
 
-				SmtpClient client = new SmtpClient();
-				client.Send(msgMail);
-			}
-			catch (Exception e)
+				try
+				{
+					msgMail.Subject = Subject;
+					msgMail.IsBodyHtml = true;
+					msgMail.Body = mailBody.ToString();
+
+					SmtpClient client = new SmtpClient();
+					client.Send(msgMail);
+				}
+				catch (Exception e)
+				{
+					throw new InvalidOperationException(
+						"Unable to send mail. Please ensure that web.config has correct /configuration/system.net/mailSettings: " +
+						e.Message);
+				}
+			}catch (Exception e)
 			{
-				throw new InvalidOperationException("Unable to send mail. Please ensure that web.config has correct /configuration/system.net/mailSettings: " + e.Message);
+				return string.Format(@"<span style=""color: red;"">{0}</span>", e.Message);
 			}
 			return ResponseText;
 		}
