487d5ff16c37f2d3f37348f4f20a46e2f0e04edd
Inna Boitsun <inna.boytsun.85@gmail.com>
2011-11-28 09:06:57 +0100

    [Composite.StarterSite.CorporateSite.Templates.MasterBased] 1.0.9 - New Version of MasterPages - http://compositec1contrib.codeplex.com/SourceControl/changeset/changes/2154e 106f751

--- Composite.StarterSite.CorporateSite.Templates.MasterBased/Package/App_Data/PageTemplates/Common.master.cs
+++ Composite.StarterSite.CorporateSite.Templates.MasterBased/Package/App_Data/PageTemplates/Common.master.cs
@@ -10,6 +10,7 @@ public partial class Common : System.Web.UI.MasterPage
 {
 	protected void Page_Load(object sender, EventArgs e)
 	{
+		html_tag.Attributes.Add("lang", System.Threading.Thread.CurrentThread.CurrentCulture.TwoLetterISOLanguageName);
 		using (var conn = new DataConnection())
 		{
 			var homepageLink = new SitemapNavigator(conn).CurrentHomePageNode.Url;
