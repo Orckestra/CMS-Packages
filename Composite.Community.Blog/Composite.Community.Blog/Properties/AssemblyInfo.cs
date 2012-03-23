036c0c4489e6e20361414a753309c1817fdfa2fa
Volodymyr I. Muzyka <info@aeont.com>
2012-03-23 09:03:00 +0100

    [Composite.Community.Blog] 1.7.5: fixed #3881 -  rebuild with 3.2. minimum version: 3.2.4464.24641

--- Composite.Community.Blog/Composite.Community.Blog/Properties/AssemblyInfo.cs
+++ Composite.Community.Blog/Composite.Community.Blog/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Composite A/S")]
 [assembly: AssemblyProduct("Composite.Community.Blog")]
-[assembly: AssemblyCopyright("Copyright © Composite A/S 2011")]
+[assembly: AssemblyCopyright("Copyright © Composite A/S 2012")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.7.3")]
-[assembly: AssemblyFileVersion("1.7.3")]
+[assembly: AssemblyVersion("1.7.5")]
+[assembly: AssemblyFileVersion("1.7.5")]
