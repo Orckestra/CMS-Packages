fd31ab15c4af6dcff9fde68c4df2785076772df1
Inna Boitsun <inna.boytsun.85@gmail.com>
2012-08-09 13:51:13 +0200

    [Composite.Community.Blog] 1.7.12 - fixed bug# 4003 - A comma is not supported in Tags

--- Composite.Community.Blog/Composite.Community.Blog/BlogCustomValidators.cs
+++ Composite.Community.Blog/Composite.Community.Blog/BlogCustomValidators.cs
@@ -10,69 +10,73 @@ using Composite.C1Console.Workflow;
 namespace Composite.Community.Blog
 {
 
-    #region Tag Validator
-    public sealed class TagValidatorAttribute : Microsoft.Practices.EnterpriseLibrary.Validation.Validators.ValueValidatorAttribute
-    {
-        protected override Microsoft.Practices.EnterpriseLibrary.Validation.Validator DoCreateValidator(Type targetType)
-        {
-            return new TagValidator();
-        }
-    }
+	#region Tag Validator
+	public sealed class TagValidatorAttribute : Microsoft.Practices.EnterpriseLibrary.Validation.Validators.ValueValidatorAttribute
+	{
+		protected override Microsoft.Practices.EnterpriseLibrary.Validation.Validator DoCreateValidator(Type targetType)
+		{
+			return new TagValidator();
+		}
+	}
 
-    public sealed class TagValidator : Microsoft.Practices.EnterpriseLibrary.Validation.Validator
-    {
-        public TagValidator()
-            : base("Tag Validation", "Tag")
-        {
-        }
-        protected override string DefaultMessageTemplate
-        {
-            get { return "Tag Validation"; }
-        }
-        // This method does the actual validation
-        protected override void DoValidate(object objectToValidate, object currentTarget, string key, Microsoft.Practices.EnterpriseLibrary.Validation.ValidationResults validationResults)
-        {
-            string tag = (string)objectToValidate;
-            using (DataConnection conn = new DataConnection())
-            {
-                var tags = conn.Get<Tags>().Where(t => t.Tag == tag).ToList();
-                if (tags.Count() > 0)
-                {
-                    Guid workflowInstanceId = WorkflowEnvironment.WorkflowInstanceId;
-                    var id = (Guid)WorkflowFacade.GetFormData(workflowInstanceId).Bindings["Id"];
-                    if (tags.Find(t => t.Id == id) == null)
-                    {
-                        LogValidationResult(validationResults, "The tag with the same name already exists", currentTarget, key);
-                    }
-                }
-            }
+	public sealed class TagValidator : Microsoft.Practices.EnterpriseLibrary.Validation.Validator
+	{
+		public TagValidator()
+			: base("Tag Validation", "Tag")
+		{
+		}
+		protected override string DefaultMessageTemplate
+		{
+			get { return "Tag Validation"; }
+		}
+		protected override void DoValidate(object objectToValidate, object currentTarget, string key, Microsoft.Practices.EnterpriseLibrary.Validation.ValidationResults validationResults)
+		{
+			var tag = (string)objectToValidate;
+			if (tag.Contains(","))
+			{
+				LogValidationResult(validationResults, "A comma is not supported", currentTarget, key);
+				return;
+			}
+			using (var conn = new DataConnection())
+			{
+				var tags = conn.Get<Tags>().Where(t => t.Tag == tag).ToList();
+				if (tags.Any())
+				{
+					Guid workflowInstanceId = WorkflowEnvironment.WorkflowInstanceId;
+					var id = (Guid)WorkflowFacade.GetFormData(workflowInstanceId).Bindings["Id"];
+					if (tags.Find(t => t.Id == id) == null)
+					{
+						LogValidationResult(validationResults, "The tag with the same name already exists", currentTarget, key);
+					}
+				}
+			}
 
-        }
-    }
+		}
+	}
 
-    public sealed class TagValidatorPropertyValidatorBuilder : PropertyValidatorBuilder<string>
-    {
-        public override Attribute GetAttribute()
-        {
-            return new TagValidatorAttribute();
-        }
+	public sealed class TagValidatorPropertyValidatorBuilder : PropertyValidatorBuilder<string>
+	{
+		public override Attribute GetAttribute()
+		{
+			return new TagValidatorAttribute();
+		}
 
-        public override CodeAttributeDeclaration GetCodeAttributeDeclaration()
-        {
-            return new CodeAttributeDeclaration(new CodeTypeReference(typeof(TagValidatorAttribute)));
-        }
-    }
+		public override CodeAttributeDeclaration GetCodeAttributeDeclaration()
+		{
+			return new CodeAttributeDeclaration(new CodeTypeReference(typeof(TagValidatorAttribute)));
+		}
+	}
 
-    public sealed class TagValidatorContainer
-    {
-        /// <summary>
-        /// Function to mapped to a C1 function in console
-        /// </summary>
-        /// <returns></returns>
-        public static PropertyValidatorBuilder<string> TagValidator()
-        {
-            return new TagValidatorPropertyValidatorBuilder();
-        }
-    }
-    #endregion
+	public sealed class TagValidatorContainer
+	{
+		/// <summary>
+		/// Function to mapped to a C1 function in console
+		/// </summary>
+		/// <returns></returns>
+		public static PropertyValidatorBuilder<string> TagValidator()
+		{
+			return new TagValidatorPropertyValidatorBuilder();
+		}
+	}
+	#endregion
 }
