ab57c7497cfb4c1fdb4311f33c90bf3e669918ad
Inna Boitsun <inna.boytsun.85@gmail.com>
2012-09-26 11:12:33 +0200

    1.0.14 - added FixUserSettingsPackageFragmentInstaller (based on http://compositec1.codeplex.com/discussions/395946 )

---
+++ Composite.DemoSites.OmniCorp/FixUserSettingsPackageFragmentInstaller/FixUserSettingsPackageFragmentInstaller.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Composite.Core.PackageSystem.PackageFragmentInstallers;
+using Composite.Core.PackageSystem;
+using Composite.Data;
+using Composite.Data.Types;
+using System.Xml.Linq;
+
+namespace PackageFragmentInstallers
+{
+	public class FixUserSettingsPackageFragmentInstaller : BasePackageFragmentInstaller
+	{
+		public override IEnumerable<XElement> Install()
+		{
+			var usersNames = new[] { "developer", "advuser", "enduser" };
+			using (var conn = new DataConnection())
+			{
+				var userSettings = conn.Get<IUserSettings>().Where(us => usersNames.Contains(us.Username));
+				var culture = DataConnection.AllLocales.First();
+				foreach (var setting in userSettings)
+				{
+					setting.CultureName = culture.Name;
+					setting.C1ConsoleUiLanguage = culture.Name;
+					setting.CurrentActiveLocaleCultureName = culture.Name;
+					setting.ForeignLocaleCultureName = culture.Name;
+				}
+				conn.Update<IUserSettings>(userSettings);
+			}
+			yield return new XElement("FixUserSettings", usersNames);
+		}
+
+		public override IEnumerable<PackageFragmentValidationResult> Validate()
+		{
+			var validationResult = new List<PackageFragmentValidationResult>();
+			return validationResult;
+		}
+	}
+}
---
+++ Composite.DemoSites.OmniCorp/FixUserSettingsPackageFragmentInstaller/FixUserSettingsPackageFragmentUnInstaller.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using Composite.Core.PackageSystem.PackageFragmentInstallers;
+using Composite.Core.PackageSystem;
+
+namespace PackageFragmentInstallers
+{
+	public class FixUserSettingsPackageFragmentUninstaller : BasePackageFragmentUninstaller
+	{
+		public override void Uninstall()
+		{
+			throw new InvalidOperationException("You can't uninstall this package");
+		}
+
+		public override IEnumerable<PackageFragmentValidationResult> Validate()
+		{
+			yield return new PackageFragmentValidationResult(PackageFragmentValidationResultType.Fatal, "Uninstall not supported");
+		}
+	}
+}
