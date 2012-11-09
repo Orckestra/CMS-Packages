2f5c956a5f4a9f9e46f070b7ac7324087f5d33ff
Inna Boitsun <inna.boytsun.85@gmail.com>
2012-11-09 09:12:34 +0100

    [Composite.Navigation.DropDown] 2.1.4 - fixed drop down on iPad - based on http://compositec1.codeplex.com/discussions/402008

--- Composite.Navigation.DropDown/Composite.Navigation.DropDown/Package/Frontend/Composite/Navigation/DropDown/Scripts/DropDown.js
+++ Composite.Navigation.DropDown/Composite.Navigation.DropDown/Package/Frontend/Composite/Navigation/DropDown/Scripts/DropDown.js
@@ -2,12 +2,18 @@ function DropDown(dropdownId, hoverClass, mouseOffDelay) {
 	if (dropdown = document.getElementById(dropdownId)) {
 		var listItems = dropdown.getElementsByTagName("li");
 		for (var i = 0; i < listItems.length; i++) {
-			listItems[i].onmouseover = function () {this.className = AddClass(this);};
-			listItems[i].onmouseout = function () {var that = this;setTimeout(function () {that.className = RemoveClass(that);}, mouseOffDelay);this.className = that.className;};
+			if ("ontouchstart" in window || "ontouch" in window) {
+				listItems[i].ontouchstart = function () { this.className = AddClass(this); };
+				listItems[i].ontouchend = function () { var that = this; setTimeout(function () { that.className = RemoveClass(that); }, mouseOffDelay); this.className = that.className; };
+			}
+			else {
+				listItems[i].onmouseover = function () { this.className = AddClass(this); };
+				listItems[i].onmouseout = function () { var that = this; setTimeout(function () { that.className = RemoveClass(that); }, mouseOffDelay); this.className = that.className; };
+			}
 			var anchor = listItems[i].getElementsByTagName("a");
 			anchor = anchor[0];
-			anchor.onfocus = function () {On(this.parentNode);};
-			anchor.onblur = function () {Off(this.parentNode);};
+			anchor.onfocus = function () { On(this.parentNode); };
+			anchor.onblur = function () { Off(this.parentNode); };
 		}
 	}
 
@@ -18,14 +24,12 @@ function DropDown(dropdownId, hoverClass, mouseOffDelay) {
 		}
 	}
 
-
 	function Off(li) {
 		if (li.nodeName == "LI") {
 			li.className = RemoveClass(li);
 			Off(li.parentNode.parentNode);
 		}
 	}
-
 
 	function AddClass(li) {
 		return li.className + " " + hoverClass;
