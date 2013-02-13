ccf281d2b251cddbb88468e4abf4a33838c64401
Taras Nakonechnyi <neexite@gmail.com>
2013-02-13 09:35:41 +0100

    Rollback changes

--- Composite.Community.TellAFriend/Composite.Community.TellAFriend/TellAFriend.asmx.cs
+++ Composite.Community.TellAFriend/Composite.Community.TellAFriend/TellAFriend.asmx.cs
@@ -3,7 +3,6 @@ using System.Web.Services;
 using System.Web.Services.Protocols;
 using System.Xml;
 using System.Xml.Linq;
-using Composite.Core.WebClient.Captcha;
 
 namespace Composite.Community.TellAFriend
 {
@@ -19,22 +18,7 @@ namespace Composite.Community.TellAFriend
 			System.Threading.Thread.CurrentThread.CurrentCulture = new System.Globalization.CultureInfo(culture);
 			System.Threading.Thread.CurrentThread.CurrentUICulture = new System.Globalization.CultureInfo(culture);
 			var send = TellAFriendFacade.Send(fromName, fromEmail, toName, toEmail, description, captcha, captchaEncryptedValue, useCaptcha, website, url);
-			var result = new XElement("TellAFriend", send);
-			if (Captcha.IsValid(captchaEncryptedValue, captcha))
-			{
-				if (captcha != url)
-				{
-					Captcha.RegisterUsage(captchaEncryptedValue);
-					var newEncryptedValue = Captcha.CreateEncryptedValue();
-					//Refactoring Captcha.GetImageUrl
-					var captchaServiceUrl = Composite.Core.WebClient.UrlUtils.PublicRootPath + "/Renderers/Captcha.ashx";
-					var captchaUrl = new Composite.Core.UrlBuilder(captchaServiceUrl);
-					captchaUrl["value"] = newEncryptedValue;
-					result.SetAttributeValue("captchaImage", captchaUrl.ToString());
-					result.SetAttributeValue("captchaEncryptedValue", newEncryptedValue);
-				}
-			}
-			return GetXmlNode(result);
+			return GetXmlNode(new XElement("TellAFriend", send));
 		}
 
 		static XmlNode GetXmlNode(XElement element)
