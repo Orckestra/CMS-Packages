80970b195d3c5cdff9fc1e06066114861fd1d7d7
Anzhelika Provoziuk <people-06@ya.ru>
2013-06-12 09:14:24 +0200

    [Composite.Navigation.Sitemap.Razor] 1.0.7: rename

--- Composite.Navigation.Sitemap.Razor/Properties/AssemblyInfo.cs
+++ Composite.Navigation.Sitemap.Razor/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Composite.Navigation.LevelSitemap.Razor")]
+[assembly: AssemblyTitle("Composite.Navigation.Sitemap.Razor")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Composite.Navigation.LevelSitemap.Razor")]
+[assembly: AssemblyProduct("Composite.Navigation.Sitemap.Razor")]
 [assembly: AssemblyCopyright("Copyright ©  2010")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
