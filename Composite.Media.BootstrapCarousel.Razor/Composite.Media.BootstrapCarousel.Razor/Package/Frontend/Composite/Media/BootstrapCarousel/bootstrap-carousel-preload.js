a2e5f4bf9d1fb45802a0e280e643b0310750d320
Anzhelika Provoziuk <people-06@ya.ru>
2013-10-08 11:35:49 +0200

    [Composite.Media.BootstrapCarousel.Razor] 2.0.1: Added version, fixed themes look.

--- Composite.Media.BootstrapCarousel.Razor/Composite.Media.BootstrapCarousel.Razor/Package/Frontend/Composite/Media/BootstrapCarousel/bootstrap-carousel-preload.js
+++ Composite.Media.BootstrapCarousel.Razor/Composite.Media.BootstrapCarousel.Razor/Package/Frontend/Composite/Media/BootstrapCarousel/bootstrap-carousel-preload.js
@@ -5,20 +5,25 @@ $(document).ready(function () {
         var images = self.find('.cover').map(function () {
             return $(this).data('src')
         }).get();
+        var indicators = $(this).find('.carousel-indicators');
 
-        $.imgpreload(images[0], function () {
+        $.imgpreload(images[0], function () { //preload first image
             self.removeClass('load-image');
 
             self.find('.item:first-child').addClass('active');
             images.shift();
 
-            $.imgpreload(images, function () {
+            $.imgpreload(images, function () { //preload all images and run carousel
                 self.carousel({
                     interval: self.data('interval')
                 });
 
             });
         });
+        
+        if (indicators.length == 0) {
+            $(this).addClass('without-indicators'); // add class to carousel without indicators
+        }
     });
 
 })
