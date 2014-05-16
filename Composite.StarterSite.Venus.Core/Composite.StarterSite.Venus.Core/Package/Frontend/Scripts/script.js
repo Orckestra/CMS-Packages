fb0b7f53dd33969e7c6df452cd2a8e0b6bb18557
Inna Boitsun <inna.boytsun.85@gmail.com>
2014-05-16 16:38:23 +0200

    fixes

--- Composite.StarterSite.Venus.Core/Composite.StarterSite.Venus.Core/Package/Frontend/Scripts/script.js
+++ Composite.StarterSite.Venus.Core/Composite.StarterSite.Venus.Core/Package/Frontend/Scripts/script.js
@@ -11,6 +11,7 @@
     /* end fix */
 
     $(document).ready(function () {
+
         $(".mega-menu").on("show.bs.dropdown", function () {
             if (!$(".navbar-collapse").hasClass("in")) {
                 showOverlay();
@@ -43,6 +44,11 @@
         var linksCount = $(".mobile-navbar-link").length > 5 ? 5 : $(".mobile-navbar-link").length;
         var linksStyle = "width-" + Math.floor(100/linksCount);
         $(".mobile-navbar-link").addClass(linksStyle);
+
+        //Profiles
+        $(".profiles-list .row").each(function () {
+            $(".thumbnail", $(this)).equalHeightColumns({minWidth: 767, extraHeight: 18});
+        });
     });
 
     function showOverlay() {
@@ -54,6 +60,16 @@
     }
 
 })(window);
+
+/* equalHeightColumns.js 1.1 */
+(function ($) {
+    $.fn.equalHeightColumns = function (options) {
+        defaults = { minWidth: -1, maxWidth: 99999, setHeightOn: "min-height", defaultVal: 0, extraHeight: 0 }; var $this = $(this); options = $.extend({}, defaults, options); var resizeHeight = function () {
+            var windowWidth = $(window).width(); if (options.minWidth < windowWidth && options.maxWidth > windowWidth) { var height = 0; var highest = 0; $this.css(options.setHeightOn, options.defaultVal); $this.each(function () { height = $(this).height(); if (height > highest) highest = height }); $this.css(options.setHeightOn, highest + options.extraHeight) } else $this.css(options.setHeightOn,
+            options.defaultVal)
+        }; resizeHeight(); $(window).resize(resizeHeight); $this.find("img").load(resizeHeight); if (typeof options.afterLoading !== "undefined") $this.find(options.afterLoading).load(resizeHeight); if (typeof options.afterTimeout !== "undefined") setTimeout(function () { resizeHeight(); if (typeof options.afterLoading !== "undefined") $this.find(options.afterLoading).load(resizeHeight) }, options.afterTimeout)
+    }
+})(jQuery);
 
 
 
