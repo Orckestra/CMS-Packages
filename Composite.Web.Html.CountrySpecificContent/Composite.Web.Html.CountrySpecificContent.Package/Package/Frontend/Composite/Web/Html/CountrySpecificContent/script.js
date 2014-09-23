56c9b7c308b368ca0bc3e1b0b7ee0c70e8513ded
Inna Boitsun <inna.boytsun.85@gmail.com>
2014-09-23 17:05:25 +0200

    [Composite.Web.Html.CountrySpecificContent] 1.0.0

---
+++ Composite.Web.Html.CountrySpecificContent/Composite.Web.Html.CountrySpecificContent.Package/Package/Frontend/Composite/Web/Html/CountrySpecificContent/script.js
@@ -0,0 +1,27 @@
+﻿(function ($) {
+    var countryCookieName = 'Composite.Web.Html.CountrySpecificContent.ClientCountry';
+
+    $(document).ready(function () {
+        var activeCountry = $.cookie(countryCookieName);
+        if (activeCountry == undefined) {
+            $.ajax({
+                url: '//freegeoip.net/json/',
+                dataType: 'json',
+                async: false,
+                success: function (json) {
+                    activeCountry = json.country_code;
+                    $.cookie(countryCookieName, activeCountry);
+                }
+            });
+        }
+
+        $(".content-for-country").each(function () {
+            var countries = $(this).data("countries").split(',');
+            if ($.inArray(activeCountry, countries) >= 0) {
+                $(this).show();
+            } else {
+                 $(this).remove();
+            }
+        });
+    });
+})(jQuery)
