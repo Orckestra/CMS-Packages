c93c6af6c50bdf8ec07ddb826d5f9ca33872527d
Dmitry Dzygin <dmitriy.dzygin@gmail.com>
2015-01-12 12:00:30 +0100

    [Composite.Web.BundlingAndMinification] 1.0.6: fixing compatibility with C1 404 functionality

---
+++ Composite.Web.BundlingAndMinification/Composite.Web.BundlingAndMinification/StartupHandler.cs
@@ -0,0 +1,19 @@
+﻿using Composite.Core.Application;
+using Composite.Core.Routing;
+
+namespace Composite.Web.BundlingAndMinification
+{
+    public static class StartupHandler
+    {
+        [ApplicationStartup]
+        public static class StatupHandler
+        {
+            public static void OnBeforeInitialize() { }
+
+            public static void OnInitialized()
+            {
+                Routes.OnBeforePageRouteAdded += routeCollection => routeCollection.Ignore("Bundles/{*pathinfo}");
+            }
+        }
+    }
+}
