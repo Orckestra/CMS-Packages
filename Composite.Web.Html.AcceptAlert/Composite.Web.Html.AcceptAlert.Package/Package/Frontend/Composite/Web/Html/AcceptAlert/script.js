4555f93b40bf949449cb9539f62c3b4ceda64622
Inna Boitsun <inna.boytsun.85@gmail.com>
2015-04-01 13:52:54 +0200

    [Composite.Web.Html.AcceptAlert] 1.0.3 - use sessionStorage instead of jqueryCookie.js and fixed bug when press OK and then resize a window

--- Composite.Web.Html.AcceptAlert/Composite.Web.Html.AcceptAlert.Package/Package/Frontend/Composite/Web/Html/AcceptAlert/script.js
+++ Composite.Web.Html.AcceptAlert/Composite.Web.Html.AcceptAlert.Package/Package/Frontend/Composite/Web/Html/AcceptAlert/script.js
@@ -6,26 +6,44 @@
         $(".accept-alert").each(function () {
             var acceptAlert = $(this).appendTo($("body"));
             var cookiesName = acceptAlertCookieName + acceptAlert.data('cookieskey');
-            var acceptAlertCookie = $.cookie(cookiesName);
-            if (acceptAlertCookie != undefined) {
+            var acceptAlertCookies = getCookies(cookiesName);
+
+            if (acceptAlertCookies == "OK") {
                 acceptAlert.remove();
                 return;
             }
+
             var okBtn = acceptAlert.find("button");
             okBtn.on("click", function () {
-                $.cookie(cookiesName, 'OK', { expires: 365 });
+                setCookies(cookiesName, "OK");
                 acceptAlert.animate({ height: '0' }, 500);
             });
-            setTimeout(function () { acceptAlert.animate({ height: acceptAlert.find(".container").innerHeight() }, 1000); }, 1000);
 
+            setTimeout(function () { acceptAlert.animate({ height: acceptAlert.find(".container").innerHeight() }, 1000); }, 1000);
         });
 
         $(window).on("resize", function () {
             $(".accept-alert").each(function () {
                 var acceptAlert = $(this);
-                acceptAlert.css("height", acceptAlert.find(".container").innerHeight());
+                var cookiesName = acceptAlertCookieName + acceptAlert.data('cookieskey');
+                var acceptAlertCookie = getCookies(cookiesName);
+                if (acceptAlertCookie != "OK") {
+                    acceptAlert.css("height", acceptAlert.find(".container").innerHeight());
+                }
             });
-
         });
+
+        function getCookies(cookiesName) {
+            if (typeof (Storage) !== "undefined") {
+                return localStorage.getItem(cookiesName);
+            }
+            return "false";
+        }
+
+        function setCookies(cookiesName, value) {
+            if (typeof (Storage) !== "undefined") {
+                localStorage.setItem(cookiesName, value);
+            }
+        }
     });
 })(jQuery)
