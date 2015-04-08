400b9dfa90e805fbaa41d89da4d0262f3660a942
Inna Boitsun <inna.boytsun.85@gmail.com>
2015-04-08 12:51:59 +0200

    [Composite.StarterSite.Bootstrap.Core] 2.0.14 - upgraded Bootstrap to 3.3.4 version

--- Composite.StarterSite.Bootstrap.Core/Composite.StarterSite.Bootstrap.Core/Package/Frontend/Scripts/script.js
+++ Composite.StarterSite.Bootstrap.Core/Composite.StarterSite.Bootstrap.Core/Package/Frontend/Scripts/script.js
@@ -1,14 +1,4 @@
 (function () {
-    // Fix responsive issue for IE 10 on Windows Phone 8
-    // Issue: https://github.com/twbs/bootstrap/issues/10497
-    if ("-ms-user-select" in document.documentElement.style && navigator.userAgent.match(/IEMobile\/10\.0/)) {
-        var msViewportStyle = document.createElement("style");
-        msViewportStyle.appendChild(
-            document.createTextNode("@-ms-viewport{width:auto!important}")
-        );
-        document.getElementsByTagName("head")[0].appendChild(msViewportStyle);
-    }
-    // end fix
 })();
 
 
