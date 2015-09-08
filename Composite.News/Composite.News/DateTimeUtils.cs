1926b07c27d7403d4fe115f10781d0b96f97b972
Dmitry Dzygin <dmitriy.dzygin@gmail.com>
2015-09-08 15:09:36 +0200

    [Composite.News] 2.2.1 Fixing a null reference in console

--- Composite.News/Composite.News/DateTimeUtils.cs
+++ Composite.News/Composite.News/DateTimeUtils.cs
@@ -1,59 +1,18 @@
 ﻿using System;
-using System.Linq;
-using Composite.Core.Routing;
-using Composite.Data;
 
 namespace Composite.News
 {
-    class NewsDataUrlMapper : IDataUrlMapper
+    static class DateTimeUtils
     {
-        public PageUrlData GetPageUrlData(IDataReference dataReference)
+        public static bool IsValidDate(int year, int month, int day)
         {
-            if (dataReference.ReferencedType != typeof(NewsItem))
-            {
-                return null;
-            }
+            if (year < DateTime.MinValue.Year || year > DateTime.MaxValue.Year)
+                return false;
 
-            var data = dataReference.Data;
+            if (month < 1 || month > 12)
+                return false;
 
-            var newsItem = data as NewsItem;
-            if (newsItem == null) return null;
-
-            var page = PageManager.GetPageById(newsItem.PageId);
-            if (page == null) return null;
-
-            var newsPathInfo = NewsFacade.GetPathInfo(newsItem.TitleUrl, newsItem.Date);
-
-            return new PageUrlData(page) { PathInfo = newsPathInfo };
-        }
-
-        public IDataReference GetData(PageUrlData pageUrlData)
-        {
-            string pathInfo = pageUrlData.PathInfo;
-
-            string[] pathInfoParts = pathInfo.Split(new[] { '/' }, StringSplitOptions.RemoveEmptyEntries);
-            if (pathInfoParts.Length != 4) return null;
-
-            if (string.IsNullOrEmpty(pathInfo)) return null;
-
-            int year, month, day;
-
-            if (int.TryParse(pathInfoParts[0], out year) 
-                && int.TryParse(pathInfoParts[1], out month) 
-                && int.TryParse(pathInfoParts[2], out day)
-                && DateTimeUtils.IsValidDate(year, month, day))
-            {
-                Guid pageId = pageUrlData.PageId;
-                string titleUrl = pathInfoParts[3];
-                var date = new DateTime(year, month, day);
-
-                var data = DataFacade.GetData<NewsItem>()
-                    .FirstOrDefault(n => n.PageId == pageId && n.Date.Date == date && n.TitleUrl == titleUrl);
-
-                return data != null ? data.ToDataReference() : null;
-            }
-
-            return null;
+            return day > 0 && day <= DateTime.DaysInMonth(year, month);
         }
     }
 }
