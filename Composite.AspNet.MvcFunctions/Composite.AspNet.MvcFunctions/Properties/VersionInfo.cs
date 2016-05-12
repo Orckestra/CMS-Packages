1ebe13088f6930fb4e659f5ad4524aeda6691b8a
Dmitry Dzygin <dmitriy.dzygin@gmail.com>
2016-05-12 11:58:00 +0200

    [Composite.AspNet.MvcFunctions] Updating assembly version on build

--- Composite.AspNet.MvcFunctions/Composite.AspNet.MvcFunctions/Properties/VersionInfo.cs
+++ Composite.AspNet.MvcFunctions/Composite.AspNet.MvcFunctions/Properties/VersionInfo.cs
@@ -1,7 +1,2 @@
-﻿using System.Reflection;
-
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.9.0")]
-[assembly: AssemblyFileVersion("1.0.9.0")]
+[assembly:System.Reflection.AssemblyVersion("1.0.10")]
+[assembly:System.Reflection.AssemblyFileVersion("1.0.10")]
