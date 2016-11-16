10b8f5b4eb608ed344b96fe77b6eb37b3d177cee
Volodymyr Muzyka <volodymyr.muzyka@westernunion.com>
2016-11-16 12:15:44 -0800

    [Composite.Web.BundlingAndMinification] Order of bundled JavaScript files are wrong #42 fixed

---
+++ Composite.Web.BundlingAndMinification/Composite.Web.BundlingAndMinification/AsIsBundleOrderer.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using System.Web.Optimization;
+
+namespace Composite.Web.BundlingAndMinification
+{
+    public class AsIsBundleOrderer : IBundleOrderer
+    {
+        public virtual IEnumerable<BundleFile> OrderFiles(BundleContext context, IEnumerable<BundleFile> files)
+        {
+            return files;
+        }
+    }
+}
