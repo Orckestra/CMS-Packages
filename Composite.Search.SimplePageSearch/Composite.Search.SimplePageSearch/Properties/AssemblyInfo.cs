8350a3506b1d778c2c87075a4838c230824c0d84
Dmitry Dzygin <dmitriy.dzygin@gmail.com>
2017-03-10 13:18:51 +0100

    [Composite.Search.SimplePageSearch] 2.4.0: Using C1 CMS v6.0 search API

--- Composite.Search.SimplePageSearch/Composite.Search.SimplePageSearch/Properties/AssemblyInfo.cs
+++ Composite.Search.SimplePageSearch/Composite.Search.SimplePageSearch/Properties/AssemblyInfo.cs
@@ -1,21 +1,15 @@
 ﻿using System.Reflection;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
 [assembly: AssemblyTitle("Composite.Search.SimplePageSearch")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Orckestra A/S")]
 [assembly: AssemblyProduct("Composite.Search.SimplePageSearch")]
-[assembly: AssemblyCopyright("Composite A/S ©  2015")]
+[assembly: AssemblyCopyright("Orckestra A/S © 2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
 [assembly: ComVisible(false)]
 
 [assembly: Guid("1859a776-f20c-4873-ac93-c7790988b77c")]
-
-[assembly: AssemblyVersion("2.2.1.0")]
-[assembly: AssemblyFileVersion("2.2.1.0")]
---
+++ Composite.Search.SimplePageSearch/Composite.Search.SimplePageSearch/SimpleSearchResult.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Composite.Search.SimplePageSearch
+{
+    public class SimpleSearchResult
+    {
+        public IReadOnlyCollection<SearchResultEntry> Entries { get; set; } = Array.Empty<SearchResultEntry>();
+        public int ResultsFound { get; set; }
+    }
+}
