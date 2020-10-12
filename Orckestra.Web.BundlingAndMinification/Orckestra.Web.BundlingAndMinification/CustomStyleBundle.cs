30e552760e234975970fd57bc1070e6b0ec80934
vadym-hyryn <57723696+vadym-hyryn@users.noreply.github.com>
2020-10-12 16:13:29 +0300

    Disable files cache for B&M #115
    Disable files cache to replace relative paths to absolute during the B&M process

--- Orckestra.Web.BundlingAndMinification/Orckestra.Web.BundlingAndMinification/CustomStyleBundle.cs
+++ Orckestra.Web.BundlingAndMinification/Orckestra.Web.BundlingAndMinification/CustomStyleBundle.cs
@@ -2,13 +2,25 @@
 
 namespace Orckestra.Web.BundlingAndMinification
 {
-	/// <summary>
-	/// Class implementing custom styles bundling and minification since WebGrease has known issues with BootStrap 4 
-	/// </summary>
-	public class CustomStyleBundle : Bundle
+    /// <summary>
+    /// Class implementing custom styles bundling and minification since WebGrease has known issues with BootStrap 4 
+    /// </summary>
+    public class CustomStyleBundle : Bundle
 	{
-		public CustomStyleBundle(string virtualPath) : base(virtualPath, new CustomCssMinify()) { }
+        public CustomStyleBundle(string virtualPath) : base(virtualPath, new CustomCssMinify()) { }
 
 		public CustomStyleBundle(string virtualPath, string cdnPath) : base(virtualPath, cdnPath, new CustomCssMinify()) { }
-	}
+
+        public override IBundleBuilder Builder
+        {
+            get
+            {
+                return base.Builder == null || base.Builder.GetType() != typeof(CustomBundleBuilder) ? new CustomBundleBuilder() : base.Builder;
+            }
+            set
+            {
+                base.Builder = value;
+            }
+        }
+    }
 }
