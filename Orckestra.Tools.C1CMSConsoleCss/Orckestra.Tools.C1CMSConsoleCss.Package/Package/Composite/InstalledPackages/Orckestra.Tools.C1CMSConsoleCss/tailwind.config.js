0e8eac02626f70d17953520b5969a4ff2d9709f9
Inna Boitsun <inna.boitsun@orckestra.com>
2021-11-30 10:50:01 +0200

    define c1 cms primary color

--- Orckestra.Tools.C1CMSConsoleCss/Orckestra.Tools.C1CMSConsoleCss.Package/Package/Composite/InstalledPackages/Orckestra.Tools.C1CMSConsoleCss/tailwind.config.js
+++ Orckestra.Tools.C1CMSConsoleCss/Orckestra.Tools.C1CMSConsoleCss.Package/Package/Composite/InstalledPackages/Orckestra.Tools.C1CMSConsoleCss/tailwind.config.js
@@ -10,7 +10,11 @@ module.exports = {
       black: colors.black,
       white: colors.white,
       gray: colors.trueGray,
-      green: colors.green,
+      primary: {
+        light: '#22b980',
+        DEFAULT: '#22b980',
+        dark: '#1ea371',
+      },
     },
     extend: {},
   },
