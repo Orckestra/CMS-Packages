c32c9c2758c8c7b54d14c4bc2e038adae69f5332
Inna Boitsun <inna.boitsun@orckestra.com>
2021-12-01 09:45:14 +0200

    add styles for Modal

--- Orckestra.Tools.C1CMSConsoleCss/Orckestra.Tools.C1CMSConsoleCss.Package/Package/Composite/InstalledPackages/Orckestra.Tools.C1CMSConsoleCss/tailwind.config.js
+++ Orckestra.Tools.C1CMSConsoleCss/Orckestra.Tools.C1CMSConsoleCss.Package/Package/Composite/InstalledPackages/Orckestra.Tools.C1CMSConsoleCss/tailwind.config.js
@@ -4,6 +4,9 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    fontFamily: {
+      'roboto': 'Roboto Condensed, sans-serif'
+    },
     colors: {
       transparent: 'transparent',
       current: 'currentColor',
