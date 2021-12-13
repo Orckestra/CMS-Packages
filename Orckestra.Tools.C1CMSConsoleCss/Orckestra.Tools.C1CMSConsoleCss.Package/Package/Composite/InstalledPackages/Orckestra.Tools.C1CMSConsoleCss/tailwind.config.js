bfd66db05f6d435967d534b3dc2b132e6d705e43
Inna Boitsun <inna.boitsun@orckestra.com>
2021-12-13 14:14:15 +0200

    Feature/orckestra tools c1cms console css (#125)
    * Create package Orckestra.Tools.C1CMSConsoleCss
    
    * remove not needed
    
    * define c1 cms primary color
    
    * fix solution file
    
    * add styles for Modal

---
+++ Orckestra.Tools.C1CMSConsoleCss/Orckestra.Tools.C1CMSConsoleCss.Package/Package/Composite/InstalledPackages/Orckestra.Tools.C1CMSConsoleCss/tailwind.config.js
@@ -0,0 +1,32 @@
+const colors = require('tailwindcss/colors')
+
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    fontFamily: {
+      'roboto': 'Roboto Condensed, sans-serif'
+    },
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      black: colors.black,
+      white: colors.white,
+      gray: colors.trueGray,
+      primary: {
+        light: '#22b980',
+        DEFAULT: '#22b980',
+        dark: '#1ea371',
+      },
+    },
+    extend: {},
+  },
+  variants: {
+    extend: {
+      opacity: ['disabled']
+    },
+  },
+  plugins: [
+    require('@tailwindcss/forms')
+  ],
+}
