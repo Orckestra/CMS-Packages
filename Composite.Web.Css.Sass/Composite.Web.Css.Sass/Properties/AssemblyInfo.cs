1994ff4fb7f098b2e789a030bfe0a3348a5b8956
vadym-hyryn <57723696+vadym-hyryn@users.noreply.github.com>
2022-07-12 12:21:45 +0300

    Fix SCSS multiinstance issue

--- Composite.Web.Css.Sass/Composite.Web.Css.Sass/Properties/AssemblyInfo.cs
+++ Composite.Web.Css.Sass/Composite.Web.Css.Sass/Properties/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Orckestra A/S")]
 [assembly: AssemblyProduct("Orckestra.Web.Css.Sass")]
-[assembly: AssemblyCopyright("Copyright © Orckestra A/S 2020")]
+[assembly: AssemblyCopyright("Copyright © Orckestra A/S 2022")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.0.2.0")]
-[assembly: AssemblyFileVersion("2.0.2.0")]
+[assembly: AssemblyVersion("2.0.4.0")]
+[assembly: AssemblyFileVersion("2.0.4.0")]
